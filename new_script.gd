5a2aa4b13a5c7e460b3aaa7d551b5f075a9d06e2
Alphen23498 <tandberg.benjamin@stu.jamesirwin.org>
2026-03-31 11:30:58 -0600

    vhcghcghcxgh

--- new_script.gd
+++ new_script.gd
@@ -1,4 +1,4 @@
-extends Node
+extends CharacterBody2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,5 +7,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	velocity .x = 0 
+	velocity .y = 0
+	if Input. is_key_pressed(Key_W):
+		velocity.y + -100
+	if Input. is_key_pressed(Key_A):
+		velocity.x = -100
+	if Input. is_key_pressed(Key_D):
+		velocity.x = 100
