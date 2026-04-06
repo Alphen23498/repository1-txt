957eede1d64274594d11235fdff39c806def3aed
Alphen23498 <tandberg.benjamin@stu.jamesirwin.org>
2026-04-06 11:25:29 -0600

    8doiit

--- new_script.gd
+++ new_script.gd
@@ -1,15 +1,22 @@
 extends CharacterBody2D
 
+@export var gravity = 10 # pixels per second per second
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
-	velocity .x = 0 
-	velocity .y = 0
-	if Input.is_key_pressed(KEY_W):
-		velocity.y = -100
+func _physics_process(delta: float) -> void:
+	
+
+	
+	velocity.y += gravity * delta
+	
+	if Input.is_key_pressed(KEY_W) and is_on_floor():
+		velocity.y = -275
+	
 
 	move_and_slide()
+	
