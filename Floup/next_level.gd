0642e652aa7365afcf7d117b633b587ad8cf17e5
Alphen23498 <tandberg.benjamin@stu.jamesirwin.org>
2026-05-05 11:29:58 -0600

    bfjgbuwbuwu

---
+++ Floup/next_level.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var next_level: PackedScene
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:	
+	if body is Player:
+		get_tree().change_scene_to_packed(next_level)
